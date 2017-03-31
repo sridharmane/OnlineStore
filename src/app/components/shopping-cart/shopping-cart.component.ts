2ea0a1e08ef4dbe1596a819ba9c2e94726b3b324
Sridhar Mane <sridhars4s@gmail.com>
2017-03-31 13:52:19 -0400

    Changed name to Online Store

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -57,7 +57,7 @@ export class ShoppingCartComponent implements OnInit {
   confirmPurchase() {
     this.cartService.confirmPurchase();
   }
-  
+
   /**
    * Removes the items from the cart
    * @param itemName
